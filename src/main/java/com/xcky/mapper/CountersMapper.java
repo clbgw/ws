fa8fba62ea5503132d25043d251867871b60a5f3
chenlongbiao <chenlongbiao@tansun.com.cn>
2022-05-16 17:53:47 +0800

    332

--- src/main/java/com/xcky/mapper/CountersMapper.java
+++ src/main/java/com/xcky/mapper/CountersMapper.java
@@ -1,10 +1,8 @@
 package com.xcky.mapper;
 
 import com.xcky.model.Counter;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-@Mapper
 public interface CountersMapper {
 
     Counter getCounter(@Param("id") Integer id);

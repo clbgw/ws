3011d4e4e78df56cba7e5a6718fc90b302e8dbaa
chenlongbiao <chenlongbiao@tansun.com.cn>
2022-05-17 09:10:27 +0800

    76

--- src/main/java/com/xcky/WxCloudRunApplication.java
+++ src/main/java/com/xcky/WxCloudRunApplication.java
@@ -6,13 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 启动接口
+ *
  * @author lbchen
  */
 @SpringBootApplication
-@MapperScan(basePackages = {"com.tencent.wxcloudrun.dao"})
-public class WxCloudRunApplication {  
-
-  public static void main(String[] args) {
-    SpringApplication.run(WxCloudRunApplication.class, args);
-  }
+@MapperScan(basePackages = {"com.xcky.mapper"})
+public class WxCloudRunApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WxCloudRunApplication.class, args);
+    }
 }
